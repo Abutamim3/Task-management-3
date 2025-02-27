094d66fefa5b371cfebbb0eb24c0c3d5ad10368e
Abdul Rahman Al-Harbi <133687927+Abutamim3@users.noreply.github.com>
2025-02-27 16:42:44 +0300

    برنامج ادارة المهام3
    برنامج ادارة المهام

---
+++ برنامج ادارة المهام/برنامج ادارة المهام/Form1.Designer.vb
@@ -0,0 +1,94 @@
+﻿Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    ' تعريف العناصر كـ WithEvents لتجنب الخطأ
+    Private WithEvents lblTitle As Label
+    Private WithEvents txtTitle As TextBox
+    Private WithEvents lblDescription As Label
+    Private WithEvents txtDescription As TextBox
+    Private WithEvents lblDueDate As Label
+    Private WithEvents dtpDueDate As DateTimePicker
+    Private WithEvents btnAdd As Button
+    Private WithEvents btnDelete As Button
+    Private WithEvents dgvTasks As DataGridView
+
+    Private Sub InitializeComponent()
+        Me.lblTitle = New System.Windows.Forms.Label()
+        Me.txtTitle = New System.Windows.Forms.TextBox()
+        Me.lblDescription = New System.Windows.Forms.Label()
+        Me.txtDescription = New System.Windows.Forms.TextBox()
+        Me.lblDueDate = New System.Windows.Forms.Label()
+        Me.dtpDueDate = New System.Windows.Forms.DateTimePicker()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.dgvTasks = New System.Windows.Forms.DataGridView()
+
+        CType(Me.dgvTasks, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+
+        ' lblTitle
+        Me.lblTitle.AutoSize = True
+        Me.lblTitle.Location = New System.Drawing.Point(12, 15)
+        Me.lblTitle.Text = "عنوان المهمة"
+
+        ' txtTitle
+        Me.txtTitle.Location = New System.Drawing.Point(100, 12)
+        Me.txtTitle.Size = New System.Drawing.Size(200, 20)
+
+        ' lblDescription
+        Me.lblDescription.AutoSize = True
+        Me.lblDescription.Location = New System.Drawing.Point(12, 45)
+        Me.lblDescription.Text = "الوصف"
+
+        ' txtDescription
+        Me.txtDescription.Location = New System.Drawing.Point(100, 42)
+        Me.txtDescription.Multiline = True
+        Me.txtDescription.Size = New System.Drawing.Size(200, 60)
+
+        ' lblDueDate
+        Me.lblDueDate.AutoSize = True
+        Me.lblDueDate.Location = New System.Drawing.Point(12, 115)
+        Me.lblDueDate.Text = "تاريخ الاستحقاق"
+
+        ' dtpDueDate
+        Me.dtpDueDate.Location = New System.Drawing.Point(100, 110)
+        Me.dtpDueDate.Size = New System.Drawing.Size(200, 20)
+
+        ' btnAdd
+        Me.btnAdd.Location = New System.Drawing.Point(100, 140)
+        Me.btnAdd.Size = New System.Drawing.Size(75, 23)
+        Me.btnAdd.Text = "إضافة"
+        Me.btnAdd.UseVisualStyleBackColor = True
+
+        ' btnDelete
+        Me.btnDelete.Location = New System.Drawing.Point(225, 140)
+        Me.btnDelete.Size = New System.Drawing.Size(75, 23)
+        Me.btnDelete.Text = "حذف"
+        Me.btnDelete.UseVisualStyleBackColor = True
+
+        ' dgvTasks
+        Me.dgvTasks.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvTasks.Location = New System.Drawing.Point(12, 180)
+        Me.dgvTasks.Size = New System.Drawing.Size(360, 150)
+
+        ' Form1
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(384, 341)
+        Me.Controls.Add(Me.dgvTasks)
+        Me.Controls.Add(Me.btnDelete)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.dtpDueDate)
+        Me.Controls.Add(Me.lblDueDate)
+        Me.Controls.Add(Me.txtDescription)
+        Me.Controls.Add(Me.lblDescription)
+        Me.Controls.Add(Me.txtTitle)
+        Me.Controls.Add(Me.lblTitle)
+        Me.Name = "Form1"
+        Me.Text = "إدارة المهام"
+
+        CType(Me.dgvTasks, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+    End Sub
+End Class
---
+++ برنامج ادارة المهام/برنامج ادارة المهام/Form1.vb
@@ -0,0 +1,89 @@
+﻿Imports System.IO
+Imports System.Windows.Forms
+
+Public Class Form1
+    Inherits System.Windows.Forms.Form
+
+    ' تعريف القائمة لتخزين المهام
+    Private TaskList As New List(Of TaskItem)
+    Private FilePath As String = Path.Combine(Application.StartupPath, "tasks.txt")
+
+    Public Sub New()
+        InitializeComponent()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        LoadTasks()
+        UpdateGrid()
+    End Sub
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        If txtTitle.Text.Trim() = "" Then
+            MessageBox.Show("يرجى إدخال عنوان المهمة.", "تحذير", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Return
+        End If
+
+        Dim newTask As New TaskItem With {
+            .Title = txtTitle.Text,
+            .Description = txtDescription.Text,
+            .DueDate = dtpDueDate.Value
+        }
+        TaskList.Add(newTask)
+        SaveTasks()
+        UpdateGrid()
+        ClearInputs()
+    End Sub
+
+    Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+        If dgvTasks.SelectedRows.Count > 0 Then
+            Dim index As Integer = dgvTasks.SelectedRows(0).Index
+            TaskList.RemoveAt(index)
+            SaveTasks()
+            UpdateGrid()
+        Else
+            MessageBox.Show("يرجى تحديد مهمة للحذف.", "تنبيه", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
+    End Sub
+
+    Private Sub LoadTasks()
+        If File.Exists(FilePath) Then
+            TaskList.Clear()
+            Dim lines As String() = File.ReadAllLines(FilePath)
+            For Each line In lines
+                Dim parts As String() = line.Split("|"c)
+                If parts.Length = 3 Then
+                    TaskList.Add(New TaskItem With {
+                        .Title = parts(0),
+                        .Description = parts(1),
+                        .DueDate = DateTime.Parse(parts(2))
+                    })
+                End If
+            Next
+        End If
+    End Sub
+
+    Private Sub SaveTasks()
+        Dim lines As New List(Of String)
+        For Each task In TaskList
+            lines.Add($"{task.Title}|{task.Description}|{task.DueDate}")
+        Next
+        File.WriteAllLines(FilePath, lines)
+    End Sub
+
+    Private Sub UpdateGrid()
+        dgvTasks.DataSource = Nothing
+        dgvTasks.DataSource = TaskList
+    End Sub
+
+    Private Sub ClearInputs()
+        txtTitle.Clear()
+        txtDescription.Clear()
+        dtpDueDate.Value = DateTime.Now
+    End Sub
+End Class
+
+Public Class TaskItem
+    Public Property Title As String
+    Public Property Description As String
+    Public Property DueDate As DateTime
+End Class
